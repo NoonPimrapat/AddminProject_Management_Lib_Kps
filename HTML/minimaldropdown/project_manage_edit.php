b715e38f11e4dfab16288e4205012290fb20154d
Rockmelody <poowanai.saithai@gmail.com>
2022-03-06 20:53:00 +0700

    form edit added

---
+++ HTML/minimaldropdown/project_manage_edit.php
@@ -0,0 +1,364 @@
+<?php
+/**
+ *  Created by PhpStorm.
+ *  User: Rock Melody
+ *  on 3/5/2022.
+ *  on 19:41 AM.
+ */
+
+    session_start();
+    include('../config/db.php');
+    $project_id = $_GET['id'];
+    $query = "SELECT * FROM project_info 
+JOIN user_details
+    ON user_details.user_id=project_info.user_id
+WHERE project_info.project_id=$project_id";
+    $result = mysqli_query($conn, $query) or die("Error in sql : $query". mysqli_error($conn));
+    $row = mysqli_fetch_array($result);
+
+
+     $queryde = "SELECT * FROM department_info" or die("Error:" . mysqli_error());
+     $departments = mysqli_query($conn, $queryde);
+
+     $queryStyle = "SELECT * FROM project_style_info ORDER BY project_style_id asc" or die("Error:" . mysqli_error());
+    // เก็บข้อมูลที่ query ออกมาไว้ในตัวแปร result .
+     $styles = mysqli_query($conn, $queryStyle);
+
+     $queryPlan = "SELECT * FROM project_plant WHERE project_id=$project_id";
+     $plans = mysqli_query($conn, $queryPlan);
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;600&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title> Minimal Dropdown Menu</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <header>
+        <!-- partial:index.partial.html -->
+        <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+
+        <div class="wrapper">
+            <div class="navbar">
+                <div class="navbar_left">
+                    <div class="logo">
+                        <img src="../img/kuthai.jpg" alt="logo ku" class="mini-logo">
+                        <img src="../img/kueng.jpg" alt="logo ku" class="mini-logo-ku">
+                    </div>
+                </div>
+
+                <div class="navbar_right">
+                    <div class="profile">
+                        <div class="icon_wrap">
+                            <img src="../img/kueng.jpg" alt="profile_pic">
+                            <span class="name"><?php if(!empty($_SESSION['user_email'])) echo $_SESSION['user_email'];?></span>
+                            <i class="fas fa-chevron-down"></i>
+                        </div>
+
+                        <div class="profile_dd">
+                            <ul class="profile_ul">
+                                <!-- logged in user information เช็คว่ามีการล็อคอินเข้ามาไหม -->
+                                <?php if (isset($_SESSION['email'])) :?>
+                                <?php endif?>
+                                <li class="profile_li"><a class="profile" href="profile.php"><span class="picon"><i
+                                                class="fas fa-user-alt"></i>
+                                        </span>Profile</a>
+                                    <div class="btn">My Account</div>
+                                </li>
+                                <li><a class="logout" href="home.php?logout='1'"><span class="picon"><i
+                                                class="fas fa-sign-out-alt"></i></span>Logout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- partial -->
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+        <script src="script.js"></script>
+    </header>
+    <header>
+        <div class="nav">
+            <ul>
+                <li class="a"><a href="operation_result.php"> สรุปการดำเนินงาน </a> </li>
+                <li class="b"><a href="#"> จัดการบุคลากร </a>
+                    <ul>
+                        <li><a href="index.php">เพิ่มบุคลากร</a></li>
+                        <li><a href="edituser.php">แก้ไข/ลบ บุคลากร</a></li>
+                    </ul>
+                </li>
+                <li class="c"><a href="project_manage.php"> จัดการโครงการ </a> </li>
+                <li class="d"><a href="project_detail.php"> จัดการรายละเอียดโครงการ </a> </li>
+            </ul>
+        </div>
+    </header>
+    <p>&nbsp&nbsp<span class="material-icons">assignment</span>จัดการโครงการ/<span
+            class="material-icons">upload_file</span>แก้ไขรายละเอียดโครงการ</p>
+
+    <form method="post" id="form" enctype="multipart/form-data" action="project_manage_edit_db.php">
+        <?php include('errors.php'); ?>
+        <?php if (isset($_SESSION['error'])) : ?>
+            <div class="error">
+                <h3>
+                    <?php
+                    echo $_SESSION['error'];
+                    unset($_SESSION['error']);
+                    ?>
+                </h3>
+            </div>
+        <?php endif ?>
+
+
+        <div class="grid-container">
+            <div class="grid-item">
+                <input type="hidden" name="project_id" value="<?php echo $_GET['id'];?>">
+                <div class="row">
+                    <div class="col-25">
+                        <label class="topic" for="project_name"> โครงการ:</label>
+                    </div>
+                    <div class="col-65">
+                        <input type="text" class="inputFill-Information" name="project_name" required value="<?php echo $row['project_name'];?>">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-25">
+                        <label class="topic" for="project_fiscal_year"> ตามแผนปฏิบัติการประจำปีงบประมาณ พ.ศ.:</label>
+                    </div>
+                    <div class="col-65">
+                        <select  name="project_fiscal_year"  class="inputFill-Information" required>
+                            <?php for($i=$row['project_fiscal_year'];$row['project_fiscal_year'] - $i <= 4;$i--): ?>
+                                    <option value="<?php echo $i ?>"><?php echo $i?></option>
+                            <?php   endfor ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-25">
+                        <label class="topic" for="project_style"> ลักษณะโครงการ:</label>
+                    </div>
+                    <div class="col-65">
+                        <?php if(($styles->num_rows > 0)): ?>
+                            <?php foreach ($styles as $style): ?>
+                                <?php if($row['project_style'] === $style['project_style_id']): ?>
+                                    <input name="project_style" type="radio" value="<?php echo $style['project_style_id'] ?>" checked><?php echo $style['project_style_name'] ?>
+                                <?php else: ?>
+                                    <input name="project_style" type="radio" value="<?php echo $style['project_style_id'] ?>" required><?php echo $style['project_style_name'] ?>
+                                <?php endif; ?>
+                            <?php endforeach;?>
+                        <?php endif;?>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-25">
+                        <label class="topic" for="project_strategy"> ภายใต้แผนยุทธศาสตร์:</label>
+                    </div>
+                    <div class="col-65">
+                        <input type="text" class="inputFill-Information" name="project_strategy" required value="<?php  if(!empty($row['project_strategy'])) echo $row['project_strategy'];?>">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-25">
+                        <label class="topic" for="routine_plan"> ภายใต้แผนงานประจำ:</label>
+                    </div>
+                    <div class="col-65">
+                        <input type="text" class="inputFill-Information" name="routine_plan" required value="<?php if(!empty($row['routine_plan']))echo $row['routine_plan'];?>">
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-25">
+                        <label class="topic" for="department_id"> ฝ่ายงาน:</label>
+                    </div>
+                    <div class="col-65">
+                        <select class="inputFill-Information" name="department_id" required>
+                            <?php if($departments->num_rows > 0):
+//                                echo $row['department_id'];exit;
+                                foreach($departments as $department): ?>
+                                    <?php if($department['department_id'] === $row['department_id']): ?>
+                                        <option selected value="<?php echo $department['department_id'] ?>"><?php echo $department['department_name']?></option>
+                                    <?php else: ?>
+                                        <option value="<?php echo $department['department_id'] ?>"><?php echo $department['department_name']?></option>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            <?php endif ?>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-25">
+                        <label class="topic" for="reason"> หลักการและเหตุผล:</label>
+                    </div>
+                    <div class="col-65">
+                        <textarea class="inputFill-Information-large" name="reason" rows="4" cols="50" required>
+                            <?php echo $row['reason'] ?>
+                        </textarea>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-25">
+                        <label class="topic" for="objective"> วัตถุประสงค์:</label>
+                    </div>
+                    <div class="col-65">
+                        <input class="inputFill-Information" name="objective" type="text" value="<?php if(!empty($row['objective'])) echo $row['objective'] ?>" required>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-25">
+                        <label for="operation" class="topic">ลักษณะการดำเนินงาน : </label>
+                    </div>
+                    <div class="col-65">
+                    <textarea name="operation" rows="4" cols="50" class="inputFill-Information-large" required>
+                        <?php if(!empty($row['objective'])) echo $row['objective'] ?>
+                    </textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-25">
+                        <label for="period_op" class="topic">ระยะเวลาดำเนินการ : </label>
+                    </div>
+                    <div class="col-65">
+                        <input type="date" id="dateStart" name="period_op" class="inputFill-Information-Datepicker"
+                               required value="<?php echo $row['period_op'] ?>">
+                        <label-inline class="topic">ถึง</label-inline>
+
+                        <input type="date" id="dateEnd" name="period_ed" class="inputFill-Information-Datepicker"
+                               required value="<?php echo $row['period_ed'] ?>">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-25">
+                        <label class="topic" for="project_place"> สถานที่:</label>
+                    </div>
+                    <div class="col-65">
+                        <input class="inputFill-Information" type="text" name="project_place" value="<?php echo $row['project_place'] ?>" required>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-25">
+                        <label for="ตัวชี้วัดโครงการ" class="topic">ตัวชี้วัดโครงการ : </label>
+                    </div>
+                    <div class="col-65">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-65">
+                        <label for="indicator_1" class="topic">1. </label>
+                        <input type="text" id="project_name" name="indicator_1" class="inputFill-Information"
+                               required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-65">
+                        <label for="indicator_value_1" class="topic">ค่าเป้าหมาย : </label>
+                        <input type="text" id="project_name" name="indicator_1_value"
+                               class="inputFill-Information-Datepicker" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-65">
+                        <label for="indicator_2" class="topic">2. </label>
+                        <input type="text" id="project_name" name="indicator_2" class="inputFill-Information"
+                               required>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-65">
+                        <label for="indicator_value_2" class="topic">ค่าเป้าหมาย : </label>
+                        <input type="text" id="project_name" name="indicator_2_value" value=""
+                               class="inputFill-Information-Datepicker" required>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-25">
+                        <label for="responsible_man" class="topic">ผู้รับผิดชอบโครงการ : </label>
+                    </div>
+                    <div class="col-65">
+                        <input type="text" name="responsible_man"
+                               value="<?php if(!empty($row['user_firstname'])) echo $row['user_firstname'] .' '. $row['user_lastname'] ?>"
+                               class="inputFill-Information" readonly>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-25">
+                        <label for="ลักษณะโครงการ : " class="topic">แผนการดำเนินงาน : </label>
+                    </div>
+                    <div class="col-65 section-table">
+                        <table class="table" style="margin-top: 25px;">
+                            <thead>
+                            <tr>
+                                <th>กิจกรรม</th>
+                                <th>ระยะเวลาดำเนินการ</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <?php if(($plans->num_rows > 0)) : ?>
+                                    <?php foreach ($plans as $plan) : ?>
+                                        <?php ?>
+                                        <tr class="box-input checked">
+                                        <td>
+                                            <input type="text" name="plant[<?php echo $plan['pid']?>][plant_detail]" value="<?php echo $plan['plant_detail']?>">
+                                            <span class="textplant_detail"></span>
+                                        </td>
+                                        <td>
+                                            <input type="text" name="plant[<?php echo $plan['pid']?>][plant_time]" value="<?php echo $plan['plant_time']?>">
+                                            <span class="textplant_time"></span>
+                                        </td>
+                                        </tr>
+                                        <?php ?>
+                                    <?php endforeach;?>
+                                <?php else: ?>
+                                    <td>
+                                        <span class="textplant_detail">ไม่มีรายการ แผนการดำเนินงาน</span>
+                                    </td>
+                                <?php endif; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="container-button">
+                    <button type="reset" value="reset" class="backButton" onclick="parent.location='index.php'">Back </button>
+                    <button type="submit" name="Update_Project" class="summitButton" >Submit</button>
+                </div>
+            </div>
+
+        </div>
+    </form></br>
+
+    <?php mysqli_close($conn); ?>
+</body>
+
+</html>
---
+++ HTML/minimaldropdown/project_manage_edit_db.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ *  Created by PhpStorm.
+ *  User: Rock Melody
+ *  on 3/6/2022.
+ *  on 2:56 PM.
+ */
+
+session_start();
+include('../config/db.php');
+
+if (isset($_POST['Update_Project'])) {
+    $project_id = $_POST['project_id'];
+    $pro_name = $_POST['project_name'];
+    $user_check_query = "SELECT * FROM project_info WHERE project_name = '$pro_name' AND project_id <> '$project_id' LIMIT 1";
+    $query = mysqli_query($conn, $user_check_query);
+    $user_id = $_SESSION['user_id'];
+    $errors = [];
+    if ($query->num_rows > 0) { // if project_name exists
+        if ($result['project_name'] === $pro_name) {
+            $errors[] = "project_name already exists";
+        }
+    }
+    $buff_update = $_POST;
+    unset($buff_update['project_id']);
+    unset($buff_update['Update_Project']);
+    unset($buff_update['plant']);
+    unset($buff_update['responsible_man']);
+    $buff_update['submit_date'] = date("Y-m-d H:i:s");
+
+    // check  required
+    $update = '';
+    foreach ($buff_update as $key => $buff_value) {
+        $buff_update[$key] = mysqli_real_escape_string($conn, trim($buff_value));
+        if(empty($buff_update[$key])) {
+            $errors[] = $key. ' is required';
+            $_SESSION['error'] = "pro_name is required";
+        }
+        if($key === array_key_last($buff_update)) {
+            // check last item
+            $update .= "{$key} = '{$buff_update[$key]}'";
+        }else{
+            $update .= "{$key} = '{$buff_update[$key]}', ";
+        }
+
+    }
+    $keys_update = array_keys($buff_update);
+
+    if(count($errors) > 0) {
+        foreach ($errors as $value) {
+            //Print the element out.
+            echo("<br>Error:");
+            echo $value;exit();
+            '<br>';
+        }
+    }else {
+        $sql = ("UPDATE project_info set {$update} WHERE project_id=$project_id");
+        $res = mysqli_query($conn, $sql);
+        if($res){
+            $_SESSION['pro_name'] = $buff_update['project_name'];
+            $_SESSION['success'] = "You are save project name";
+            $_SESSION['project_id'] = $project_id;
+            // แผนการดำเนินงาน
+            if(!empty($_POST['plant'])) {
+                foreach ($_POST['plant'] as $pid => $plant) {
+                    $values = "'{$plant['plant_detail']}','{$plant['plant_time']}'";
+                    $sql = "UPDATE project_plant SET plant_detail = '{$plant['plant_detail']}', plant_time = '{$plant['plant_time']}' WHERE pid = $pid";
+                    $resPlant = mysqli_query($conn, $sql);
+                }
+            }
+            // send line noti
+            line_noti("\nมีการแก้ไขอนุมัติโครงการ\nโครงการ: {$_POST['project_name']}\nเริ่ม: {$_POST['period_op']}\nสิ้นสุด: {$_POST['period_ed']}");
+            // header("location:approval_confirm.php");exit;
+//            echo("<script>location.href ='/Project_Management_Lib_Kps/approval_confirm.php?';</script>");
+        }
+    }
+}
+
+function line_noti($msn)
+{
+    $status = false;
+    $message = "404 error.";
+    if ($msn) :
+
+        $curl = curl_init();
+        $LINE_API_KEY = "LIBWf00oYPIzo4pUDKherAXCQfCiS5NLnE6b8i409eH";//ใส่Key
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://notify-api.line.me/api/notify",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "message={$msn}",
+            CURLOPT_HTTPHEADER => array(
+                "authorization: Bearer {$LINE_API_KEY}",
+                "content-type: application/x-www-form-urlencoded"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            $message = "cURL Error #:" . $err;
+        } else {
+            $status = true;
+            $message = "Success";
+        }
+    endif;
+    echo json_encode(array('status' => $status, 'message' => $message, 'response' => $response));
+}
